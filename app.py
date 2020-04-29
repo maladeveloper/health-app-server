ea62650bdb2cd8b147450ba5cabfd19be92da9e7
Malavan Srikumar <msri0004@student.monash.edu>
2020-04-29 15:29:41 +1000

    Returns a random Encounter's patients

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import jsonify
 import requests
+import functions
 
 app = Flask(__name__)
 
@@ -9,14 +10,13 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
-@app.route('/example')
-def example():
+@app.route('/associatedPatients')
+def returnAssociatedPatients():
     ##Just testing to see if i can make these kinds of API requests anyway
-    root_url = 'https://fhir.monash.edu/hapi-fhir-jpaserver/fhir/'
-    patients_url = root_url + "Patient"
-    patient_id_url = patients_url + "/1"
-    data = requests.get(url=patient_id_url).json()
-    return data
+
+    patientIDArray = functions.returnPatientIDArray(str(5059914))
+    ###NOW I CAN SEARCH THROUGH ALL THE EnCOUNTERS
+    return jsonify(patientIDArray)
 
 if __name__ == '__main__':
     app.run()
---
+++ functions.py
@@ -0,0 +1,42 @@
+import requests
+def returnPatientIDArray(practitioner_id):
+    root_url = 'https://fhir.monash.edu/hapi-fhir-jpaserver/fhir/'
+    search1_url = root_url +"Encounter?practitioner="+str(practitioner_id)
+
+    patientIDArray =[]
+    #Now search all the encounters and append patient IDs to patient ID array
+    next_page = True
+    next_url = search1_url
+    count_page = 0
+    count_patient = 0
+    count_encounters = 0
+
+    while next_page:
+        dReports = requests.get(url=next_url).json()
+
+        # As discussed before, The monash FHIR server return results in a series of pages.
+        # Each page contains 10 results as default.
+        # here we check and record the next page
+        next_page = False
+        links = dReports['link']
+        for i in range(len(links)):
+            link = links[i]
+            if link['relation'] == 'next':
+                next_page = True
+                next_url = link['url']
+                count_page += 1
+
+        # Extract data
+        entry = dReports['entry']
+        for i in range(len(entry)):
+            count_encounters+=1
+            prac_returned_id =str(entry[i]['resource']['participant'][0]['individual']['reference'].split('/')[1])
+            print("PAGE: "+str(count_page))
+            print("ENCOUNT:"+str(count_encounters))
+            #Find only the encounters in which this specific practitioner was involved.
+            if practitioner_id == prac_returned_id:
+                #Get the patient ID from such an entry
+                patientID = entry[i]['resource']['subject']['reference'].split('/')[1]
+                patientIDArray.append(patientID)
+
+    return patientIDArray
