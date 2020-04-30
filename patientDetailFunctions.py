14adcc2f8e969ec37bbea260c79cd5032fbf5d3d
Malavan Srikumar <msri0004@student.monash.edu>
2020-04-30 10:49:27 +1000

    added error checking for observation

--- patientDetailFunctions.py
+++ patientDetailFunctions.py
@@ -2,18 +2,33 @@ import requests
 
 
 def returnPatientCholesterolLevel(patientID):
+    cholesterol_details_dict = {}
+    def returnNUllCholesterolData():
+        cholesterol_details_dict["cholesterol_data"] = "NONE"
+        cholesterol_details_dict["cholesterol_units"]= "NONE"
+        cholesterol_details_dict["time_issued"]= "NONE"
+        return cholesterol_details_dict
+
     root_url = 'https://fhir.monash.edu/hapi-fhir-jpaserver/fhir/'
     search3_url = root_url + "Observation?patient="+patientID+"&code=2093-3&_sort=-date"
-    data2 = requests.get(url=search3_url).json()
-    ##not all patients might have cholesterol data so we must try-catch
+    data = requests.get(url=search3_url)
+    ##not all patients might have cholesterol data so we must error check
+    print(data.status_code)
+    if data.status_code!="200":
+        return returnNUllCholesterolData()
+
+    data2 = data.json()
+
     entry = data2['entry']
 
     #Since we sorted by newest date, we get the first enty from the entiries array
-    cholesterol_details_dict = {}
+
     #Make sure there is an entry
     if len(entry)>=1:
         cholesterol_details_dict["cholesterol_data"] = str(entry[0]['resource']['valueQuantity']['value'])
         cholesterol_details_dict["cholesterol_units"] = str(entry[0]['resource']['valueQuantity']['unit'])
         cholesterol_details_dict["time_issued"] = str(entry[0]['resource']['issued'])
+    else:
+        return returnNUllCholesterolData()
 
     return cholesterol_details_dict
